15e35987e3d0898c334b228bfb7a3176bc22ae21
David Öjebrink Olssson <David.ojebrinkolsson@elev.ga.ntig.se>
2023-08-31 10:30:37 +0200

    rthryhjtydtyyt

---
+++ lucktext/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("tja hur mår du chomp!");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Ta en frooze och du mår 1000 gånger bättre chomp!");
+
+string nam = Console.ReadLine();
+
+Console.WriteLine();
+
+string na = Console.ReadLine();
+
+Console.WriteLine("Du ska ta emot den annars!");
+
+string n = Console.ReadLine();
+
+Console.WriteLine("Om du fortrfarande inte tar emot jag kommer skicka viktor på dig");
+
+
+Console.WriteLine();
+
+
+
+Console.ReadLine();
